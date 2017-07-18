c145048860b7143489f53f6719610a09a0ec6ec7
Saravanakumar <saravanakumargn@gmail.com>
2017-07-18 20:35:28 +0530

    changes

--- web/assets/scripts/admin-page/index.js
+++ web/assets/scripts/admin-page/index.js
@@ -13,6 +13,8 @@ studyApp.controller('CourceController', ['$scope', function ($scope) {
         vm.repos = [];
         vm.querySearch = querySearch;
         vm.selectedItemChange = selectedItemChange;
+
+        vm.branchesText = '';
 //    vm.selectedItem = {"id":"83242298-6951-11e7-958e-b083fea7641b","bn":"civil engg. and planning","bc":"CP"}
 //    vm.searchTextChange   = searchTextChange;
         function defaultAll() {
@@ -25,40 +27,21 @@ studyApp.controller('CourceController', ['$scope', function ($scope) {
                         "bn": "agricultural and irrigation engineering (ss)",
                         "bc": "AI"
                     },
+                    "bc": "AI",
                     "st": 2002
                 },
                 {
                     "course": {
                         "id": "83242298-6951-11e7-958e-b083fea7641b", "bn": "civil engg. and planning", "bc": "CP"
                     },
+                    "bc": "CP",
                     "st": 2012
                 },
                 {
                     "course": {
                         "id": "8346c874-6951-11e7-958e-b083fea7641b", "bn": "production engineering (sandwich) (ss)", "bc": "PS"
                     },
-                    "st": 2010
-                }
-            ];
-            vm.cource.branchModal1 = [
-                {
-                    "course": {
-                        "id": "831b581c-6951-11e7-958e-b083fea7641b",
-                        "bn": "agricultural and irrigation engineering (ss)",
-                        "bc": "AI"
-                    },
-                    "st": 2002
-                },
-                {
-                    "course": {
-                        "id": "83242298-6951-11e7-958e-b083fea7641b", "bn": "civil engg. and planning", "bc": "CP"
-                    },
-                    "st": 2012
-                },
-                {
-                    "course": {
-                        "id": "8346c874-6951-11e7-958e-b083fea7641b", "bn": "production engineering (sandwich) (ss)", "bc": "PS"
-                    },
+                    "bc": "PS",
                     "st": 2010
                 }
             ];
@@ -87,7 +70,7 @@ studyApp.controller('CourceController', ['$scope', function ($scope) {
         function loadAll(allBranches) {
 
             return allBranches.map(function (repo) {
-                repo.bn = repo.bn.toLowerCase();
+//                repo.bn = repo.bn.toLowerCase();
                 return repo;
             });
         }
@@ -96,7 +79,7 @@ studyApp.controller('CourceController', ['$scope', function ($scope) {
             console.log('Item changed to ' + JSON.stringify(item));
             vm.cource.branchModal[index].course = item;
         }
-        
+
 //                vm.cource.branchModal.push({
 //                    "course": {
 //                        "id": "831b581c-6951-11e7-958e-b083fea7641b",
@@ -112,7 +95,7 @@ studyApp.controller('CourceController', ['$scope', function ($scope) {
 //                    },
 //                    "st": 2012
 //                });
-        
+
         function querySearch(query) {
             var results = query ? vm.repos.filter(createFilterFor(query)) : vm.repos;
             return results;
@@ -132,11 +115,60 @@ studyApp.controller('CourceController', ['$scope', function ($scope) {
         }
         function createFilterFor(query) {
             var lowercaseQuery = angular.lowercase(query);
-
             return function filterFn(item) {
-//console.log(item)
-                return (item.bn.indexOf(lowercaseQuery) === 0);
+//                console.log(item.bn+" "+item.bc)
+                return (item.bn.toLowerCase().indexOf(lowercaseQuery) === 0);
             };
-
         }
+        function createFilterPaste(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(item) {
+                return (item.bc.indexOf(lowercaseQuery) === 0);
+            };
+        }
+
+        $scope.$watch('vm.branchesText', function (newValue, oldValue) {
+            console.log(newValue)
+            // console.log(val);
+            var columns = newValue.split(/\r?\n/);
+            // console.log(columns);
+            for (var i = 0; i < columns.length; i++) {
+                if (columns[i]) {
+                    var courseAray = columns[i].trim();
+                    // console.log(name);
+                    var items = courseAray.split(/\r?\t/);
+                    // console.log(items)
+                    console.log('*********');
+                    var branchObj = {};
+                    var index = 0;
+                    for (var j = 0; j < items.length; j++) {
+                        var item = items[j].trim();
+                        if (item.length > 0) {
+                            
+                            console.log(item +" "+index);
+                            switch (index) {
+                                case 0:
+//                                    branchObj = {                                        
+//                                        "course": vm.repos.filter(createFilterFor(item))
+//                                    };
+                                    branchObj["course"] = createFilterPaste(item);
+                                    break;
+                                case 1:
+                                    branchObj["approved"] = item;
+                                    break;
+                                case 2:
+                                    branchObj["st"] = item;
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                            }
+index++;
+                        }
+                    }
+                    console.log(branchObj);
+                }
+            }
+        });
     }]);
--- web/assets/scripts/admin-page/scripts.js
+++ web/assets/scripts/admin-page/scripts.js
@@ -1,47 +1,65 @@
 function collegeChange(val) {
 //   console.log(val)
-  var columns = val.split(/\r?\n/);
+    var columns = val.split(/\r?\n/);
 //   console.log(columns);
-  //for(i=0; i < columns.length; i++){
-  for(i=0; i < 4; i++){
-    if(columns[i]) {
-    var name = columns[i].trim();
+    //for(i=0; i < columns.length; i++){
+    for (i = 0; i < 4; i++) {
+        if (columns[i]) {
+            var name = columns[i].trim();
 //     console.log(name);    
-      switch(i) {
-        case 0:
-          if(name!== '--' && name.length>=5) {
-          $('#cPhoneNo').val(name);
-          }
-          else {
-            $('#cPhoneNo').val('');           
-          }
-          break;
-        case 1:
-          if(name!== '--' && name.length>=5) {
-          $('#cFaxNo').val(name);           
-          }
-          else {
-            $('#cFaxNo').val('');           
-          }
-          break;
-        case 2:
-          if(name!== '--' && name.length>=5) {
-          $('#cEmailId').val(name);
-          }
-          else {
-            $('#cEmailId').val('');           
-          }
-          break;
-        case 3:
-          if(name!== '--' && name.length>=5) {
-          $('#cWeb').val(name);
-          }
-          else {
-            $('#cWeb').val('');           
-          }
-          break;
-      }
+            switch (i) {
+                case 0:
+                    if (name !== '--' && name.length >= 5) {
+                        $('#cPhoneNo').val(name);
+                    } else {
+                        $('#cPhoneNo').val('');
+                    }
+                    break;
+                case 1:
+                    if (name !== '--' && name.length >= 5) {
+                        $('#cFaxNo').val(name);
+                    } else {
+                        $('#cFaxNo').val('');
+                    }
+                    break;
+                case 2:
+                    if (name !== '--' && name.length >= 5) {
+                        $('#cEmailId').val(name);
+                    } else {
+                        $('#cEmailId').val('');
+                    }
+                    break;
+                case 3:
+                    if (name !== '--' && name.length >= 5) {
+                        $('#cWeb').val(name);
+                    } else {
+                        $('#cWeb').val('');
+                    }
+                    break;
+            }
+        }
     }
-  }
-  
+
+}
+
+
+function branchesChange(val) {
+    // console.log(val);
+    var columns = val.split(/\r?\n/);
+    // console.log(columns);
+    for (i = 0; i < columns.length; i++) {
+        if (columns[i]) {
+            var courseAray = columns[i].trim();
+            // console.log(name);
+            var items = courseAray.split(/\r?\t/);
+            // console.log(items)
+            console.log('*********')
+            for (j = 0; j < items.length; j++) {
+                var item = items[j].trim();
+                if (item.length > 0) {
+                    console.log(item)
+                }
+            }
+        }
+    }
 }
